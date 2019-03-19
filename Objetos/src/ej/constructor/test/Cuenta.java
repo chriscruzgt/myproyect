19e32fedd82881d7438d060e59755050f1807f8a
nante <nante@192.168.0.10>
2019-03-19 14:24:26 -0600

    objeto de cuentas de usuario del banco

---
+++ Objetos/src/ej/constructor/test/Cuenta.java
@@ -0,0 +1,20 @@
+package ej.constructor.test;
+
+public class Cuenta {
+	public int getMonetario() {
+		return monetario;
+	}
+	public void setMonetario(int monetario) {
+		this.monetario = monetario;
+	}
+	public int getAhorro() {
+		return ahorro;
+	}
+	public void setAhorro(int ahorro) {
+		this.ahorro = ahorro;
+	}
+	private  int monetario;
+	private int ahorro;
+	
+	
+}
